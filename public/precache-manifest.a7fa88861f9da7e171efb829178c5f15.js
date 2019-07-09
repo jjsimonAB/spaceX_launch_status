63e159c2b9361ce80c8f9b34f9b8cbdbec5f985d
Jeffry Simon <simon.dev93@gmail.com>
2019-07-08 22:07:58 -0400

    generationg static assets

---
+++ public/precache-manifest.a7fa88861f9da7e171efb829178c5f15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b862985b679b682aa25729c586e843d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4d3fe5807e42ac86dd6",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1893d1c5c9a48d79ce7c",
+    "url": "/static/js/2.5158e3af.chunk.js"
+  },
+  {
+    "revision": "e4d3fe5807e42ac86dd6",
+    "url": "/static/js/main.afe36737.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
